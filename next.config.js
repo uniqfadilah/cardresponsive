e16b5a0d4dc4f88549cd2511db6f67e6edb31cf2
uniqfadilah <uniqfadilah@gmail.com>
2023-03-30 03:55:04 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['images.prismic.io'],
+  },
   experimental: {
     appDir: true,
+
   },
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,91 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+'use client'
+import Flag from "@/components/flag";
+import Image from "next/image";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<any>({})
+  const [descVisibility, setDescVisibility] = useState<boolean>(false);
+  const [numberVisible, setNumberVisible] = useState<boolean>(true)
+
+  const fetchData = async () => {
+    const response = await fetch("/mock.json");
+    const jsonData = await response.json();
+    setData(jsonData)
+  };
+  useEffect(() => {
+
+    fetchData();
+  }, []);
+  const formatCurrency = (amount: number): string => {
+    return amount.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 0,
+    });
+  }
+
+  const handleAnonymize = useMemo(() => {
+    if (!data.description) return ""
+    return data.description.replace(/\d{4}\s?\d{4}/g, (match: any) => {
+      const firstFourDigits = match.substring(0, 4);
+      const realNumber = match.replace(/\s/g, ""); // Removes space from real number
+      return `<span class="text-blue-500 hover:text-blue-700 cursor-pointer" realNumber="${realNumber}" >${firstFourDigits} XXXX</span>`;
+    });
+  }, [data.description])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='container mx-auto flex  h-screen items-center justify-center'>
+      {data.pic && <div className="w-[343px] sm:w-[554px] shadow bg-white   flex flex-col relative">
+        <div className="absolute -left-2 top-1"><Flag /></div>
+        <div className="flex   rounded-t overflow-hidden">
+          <Image src={data?.pic[0]} alt="img" width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%', height: '272px', objectFit: "cover" }}
+            className="w-full "
+
+          />
         </div>
-      </div>
+        <div className="p-[16px] sm:p-[24px] ">
+          <div className="bg-white  flex sm:flex-row flex-col gap-1 sm:gap-2">
+            <div className="  flex-1 text-[#1A2258]">
+              <div className="flex space-x-4 items-center">
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+                <i className="i-building" />
+                <div>
+                  <p className="font-bold text-[16px] sm:text-xl">{data.title}</p>
+                  <p className="text-xs sm:text-sm text-[#787D9C]">{data.address}</p>
+                </div>
+              </div>
+              <div className="py-[12px] leading-snug text-sm sm:text-base">
+                <p>{data.project_type} · {data.year} · {data.ownership_type}</p>
+                <p>{data.availabilities_label}</p>
+              </div>
+
+
+            </div>
+
+            <div className="flex sm:flex-col sm:items-end items-center space-x-2 sm:space-x-0 ">
+              <p className="font-bold text-base sm:text-[19px]">{formatCurrency(data.psf_min)} - {formatCurrency(data.psf_max)} psf</p>
+              <p className="text-xs sm:text-sm text-[#787D9C] text-right">{data.subprice_label}</p>
+            </div>
+
+          </div>
+          <div className="flex justify-end mt-2">
+            <button onClick={() => setDescVisibility((e) => !e)} className="font-semibold text-[#1757D7] text-sm sm:text-base">See description</button>
+          </div>
+
+          <div onClick={(e) => { setNumberVisible((e) => !e) }} className={`text-sm sm:text-base mt-2 ${descVisibility ? '' : 'hidden'}`} dangerouslySetInnerHTML={{ __html: numberVisible ? handleAnonymize : data.description }} />
+
+
+
         </div>
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </div>}
+
+    </main >
   )
 }
